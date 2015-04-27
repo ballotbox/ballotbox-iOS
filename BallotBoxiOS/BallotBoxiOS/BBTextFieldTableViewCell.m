4802c8c8efb46017700e596f1bc1e75706a411d2
James Grippo <jgrippo95@gmail.com>
2015-04-26 23:08:14 -0400

    Got rid of storyboard

---
+++ BallotBoxiOS/BallotBoxiOS/BBTextFieldTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  BBTextFieldTableViewCell.m
+//  BallotBoxiOS
+//
+//  Created by james grippo on 4/25/15.
+//  Copyright (c) 2015 james grippo. All rights reserved.
+//
+
+#import "BBTextFieldTableViewCell.h"
+
+@implementation BBTextFieldTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ BallotBoxiOS/BallotBoxiOS/Create Election/BBAddTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  BBAddTableViewCell.m
+//  BallotBoxiOS
+//
+//  Created by james grippo on 4/25/15.
+//  Copyright (c) 2015 james grippo. All rights reserved.
+//
+
+#import "BBAddTableViewCell.h"
+
+@implementation BBAddTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
